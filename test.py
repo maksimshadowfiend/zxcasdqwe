5b05f6205327573c42756fabee6904d509607de9
maksimshadowfiend <tehnomaks2@gmail.com>
2025-11-14 14:42:25 +0000

    свое напиши

---
+++ test.py
@@ -0,0 +1,13 @@
+print("Hello world!")
+
+                  
+a = [15, 11, 32, 55, 3222, 31, 55, 323]
+a.sort()
+print("max:", a[-1])
+
+
+                 
+import math
+result = math.factorial(5)
+print(result)
+    
